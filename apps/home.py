d5f2a4d51b55850cd5b03d84322d2f38d824dbd2
dokeefemain <dokeefe3@student.gsu.edu>
2021-04-21 19:58:05 -0400

    Adding the ability to view your posts once logged in, this helps us out with primary keys

--- apps/home.py
+++ apps/home.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pymongo
 from datetime import datetime
 def app():
+    #
     myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017")
     db = myclient['millennials']
     users = db['users']
@@ -18,6 +19,17 @@ def app():
         user_pass = users.find( {"username": username_in} )[0].get("pass")
         if pass_in == user_pass:
             st.write('Correct password')
+            threads = posts.find({'username': username_in}).sort("creationdate", -1)
+            st.header('Posts created by '+username_in)
+            for x in threads:
+                
+                st.write("Post ID",x.get("creationdate"))
+                title = x.get("title")
+                body = x.get("content")
+                comment = comments.find({"post_id": str(x.get("creationdate"))})
+                count = 0
+                st.write(title)
+                st.write("--",body)
         else:
             st.write('Incorrect password')
 
